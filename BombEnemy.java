ae32c55e5ada0520faa16a15cebeb74c7d45581f
Bryan Tamada <btamada@users.noreply.github.com>
2016-06-19 00:35:40 -0700

    Given a 2D grid kill as many enemies as possible with a single bomb

---
+++ BombEnemy.java
@@ -0,0 +1,99 @@
+/**
+
+ We have a 2D grid. Each cell is either a wall, an enemy or empty.
+
+ For example (0-empty, X-enemy, Y-wall):
+
+ 0 X 0 0
+ X 0 Y X
+ 0 X 0 0
+
+ You have one bomb and you want to kill as many enemies as possible with it.
+ The bomb will kill all the enemies in the same row and column from the planted point until it hits the wall
+ since the wall is too strong to be destroyed.
+ Given such a grid, return the maximum enemies you can kill with one bomb.
+ Note that you can only put the bomb at empty cell.
+ In the example, if you put a bomb at (1,1) you will kill 3 enemies which is the best you can get.
+ You can not kill the guy behind the wall at (1,3).
+
+ */
+public class BombEnemy {
+
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'0', 'X', '0', '0'},
+            {'X', '0', 'Y', 'X'},
+            {'0', 'X', '0', '0'}
+        };
+        System.out.println(bombEnemy(grid));
+    }
+
+    /*
+        Algorithm Design:
+        - For each '0' in the grid place the bomb
+            - move along the column until you either hit a wall ('Y') or the end of the column
+            - move along the row until you either hit a wall ('Y') or the end of the row
+            - keep a counter of how many enemies you kill as you iterate through each row/col
+            - use Math.max() to compare the maxEnemiesKilled with the running total of currEnemiesKilled
+                - put the result (the max of the two) into maxEnemiesKilled variable
+        - return maxEnemiesKilled
+     */
+    private static int bombEnemy(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int maxEnemiesKilled = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int k = 0; k < grid[0].length; k++) {
+
+                // found an empty cell
+                if(grid[i][k] == '0') {
+
+                    // keep track of number of enemies killed in this iteration
+                    int currEnemiesKilled = 0;
+
+                    // search through left-row
+                    currEnemiesKilled += searchRow(i,"left");
+
+                    // search through right-row
+                    currEnemiesKilled += searchRow(i,"right");
+
+                    // search through up-col
+                    currEnemiesKilled += searchCol(k,"up");
+
+                    // search through down-col
+                    currEnemiesKilled += searchCol(k,"down");
+
+                    // update the maximum enemies killed
+                    maxEnemiesKilled = Math.max(currEnemiesKilled,maxEnemiesKilled);
+                }
+            }
+        }
+        return maxEnemiesKilled;
+    }
+
+    private static int searchRow(int rowNum, String dir) {
+
+        int numEnemiesKilled = 0;
+
+        if(dir.equalsIgnoreCase("left")) {
+
+        } else if(dir.equalsIgnoreCase("right")) {
+
+        }
+
+        return numEnemiesKilled;
+    }
+
+    private static int searchCol(int colNum, String dir) {
+
+        int numEnemiesKilled = 0;
+
+        if(dir.equalsIgnoreCase("up")) {
+
+        } else if(dir.equalsIgnoreCase("down")) {
+
+        }
+
+        return numEnemiesKilled;
+    }
+
+}
